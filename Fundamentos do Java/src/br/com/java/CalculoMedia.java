38468928ce45ac18fb97ba6e793af5b9eed993bf
sabrina.sviana <sabrina.sviana@TAT0541214W10-1.senacsp.edu.br>
2018-05-23 16:13:00 -0300

    Adicionada a Classe CalcularMedia

---
+++ Fundamentos do Java/src/br/com/java/CalculoMedia.java
@@ -0,0 +1,27 @@
+package br.com.java;
+
+import java.util.Scanner;
+
+public class CalculoMedia {
+
+	public static void main(String[] args) {
+		//A linha abaixo cria variaveis do tipo real
+		 float nota1, nota2, media;
+		// Entrada
+		System.out.print("Digite a nota1: ");
+		// a linha abaixo cria um objeto de nome teclado usando o Scanner
+		Scanner teclado = new Scanner (System.in);
+		// a linha abaixo usa o objeto teclado para capturar o que foi
+		// digitado no console e armazenar na variável nota1
+		nota1 = teclado.nextFloat();
+		System.out.print("Digite a nota2: ");
+		nota2 = teclado.nextFloat();
+		//processamento
+		media = (nota1+ nota2) / 2;
+		//saida
+		// + concatena(junta) um texto com o conteudo da variavel media
+		System.out.println("Média Final: " + media);
+
+	}
+
+}
