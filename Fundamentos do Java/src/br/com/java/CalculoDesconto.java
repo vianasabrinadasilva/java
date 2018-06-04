9ac4fa20b6d6aa42a05f47517141e5e0d5647920
sabrina.sviana <sabrina.sviana@TAT0541214W10-1.senacsp.edu.br>
2018-06-04 14:02:59 -0300

    Regra de 3

---
+++ Fundamentos do Java/src/br/com/java/CalculoDesconto.java
@@ -0,0 +1,24 @@
+package br.com.java;
+
+import java.util.Scanner;
+
+public class CalculoDesconto {
+
+	public static void main(String[] args) {
+		double preço,desconto,total;
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("Desconto do Produto");
+		System.out.println("desconto % de preço = total");
+		//entrada
+		System.out.print("Digite o preço = ");
+		preço = teclado.nextDouble();
+		System.out.println("Digite o Desconto% = ");
+		desconto = teclado.nextDouble();
+		//processamento
+		total = (preço * desconto) / 100;
+		//saida
+		System.out.println(preço + "% de " + desconto + " = " + total);
+
+	}
+
+}
