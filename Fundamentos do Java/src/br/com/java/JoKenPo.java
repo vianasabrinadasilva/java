9209ac5a4b4011194bb785f3fa1478c5db1c70de
sabrina.sviana <sabrina.sviana@TAT0541214W10-1.senacsp.edu.br>
2018-06-13 16:07:30 -0300

    Terminado

--- Fundamentos do Java/src/br/com/java/JoKenPo.java
+++ Fundamentos do Java/src/br/com/java/JoKenPo.java
@@ -13,7 +13,9 @@ import java.util.Scanner;
 public class JoKenPo {
 
 	public static void main(String[] args) {
-		int jogador, placar = 0;
+		int jogador;
+		int player1 = 0;
+		int player2 = 0;
 		char opcao;
 		Scanner teclado = new Scanner(System.in);
 		do {
@@ -69,16 +71,18 @@ public class JoKenPo {
 		if (jogador == 1 && computador == 3 || jogador == 2 && computador == 1 || jogador == 2 && computador == 3 ) {
 			System.out.println("----------------------------");
 			System.out.println("******JOGADOR VENCEU******");
+			player1++;
+			System.out.println("PLACAR: " + player1 + "X" + player2);
 		}else if (computador == 1 && jogador == 3 || computador == 2 && jogador == 1 || computador == 2 && jogador == 3 ) {
 			System.out.println("----------------------------");
 			System.out.println("******COMPUTADOR VENCEU*******");
+			player2++; 
+			System.out.println("PLACAR: " + player1 + "X" + player2);
 		} else {
 			System.out.println("----------------------------");
 			System.out.println("******EMPATE******");
+			System.out.println("PLACAR: " + player1 + "X" + player2);
 		}
-		
-		System.out.println("----------------------------");
-		System.out.println("¨¨¨¨¨¨PLACAR¨¨¨¨¨¨");
 		
 		System.out.print("Deseja jogar novamente(s/n)? ");
 		opcao = teclado.next().charAt(0);
