79bff70495a29e1ad7c674b115111952bf1faa16
sabrina.sviana <sabrina.sviana@TAT0541214W10-1.senacsp.edu.br>
2018-06-20 13:54:29 -0300

    CALCULO IMC

--- Fundamentos do Java/src/br/com/java/CalculoIMC.java
+++ Fundamentos do Java/src/br/com/java/CalculoIMC.java
@@ -5,27 +5,32 @@ import java.util.Scanner;
 public class CalculoIMC {
 
 	public static void main(String[] args) {
-	double altura,peso,imc;
-	Scanner teclado = new Scanner(System.in);
-	System.out.println("Calculo do IMC");
-	System.out.print("Digite o seu peso: ");
-	peso = teclado.nextDouble();
-	System.out.print("Digite a sua altura: ");
-	altura = teclado.nextDouble();
-	imc = peso / (altura * altura);
-	System.out.println("O seu IMC é: " + imc);
-	if (imc < 16) {
-		System.out.println("Muito abaixo do peso");
-	} else if (imc == 17 || imc == 18.49) {
-		System.out.println("Abaixo do peso");
-	} else if (imc == 18.5 || imc == 24.99) {
-		
-		
-	} else {
+		double peso, altura, imc;
+		System.out.println("Digite o seu peso em kg: ");
+		//entrada 
+		Scanner teclado = new Scanner(System.in);
+		peso = teclado.nextDouble();
+		System.out.println("Digite sua altura em metros: ");
+		altura = teclado.nextDouble();
+		// calculo
+		imc = peso / (altura * altura);
+		//saída
+        System.out.println(" o valor da sua massa corporal é: " + imc);
+        System.out.println("");
+        if (imc < 18.49) {
+			System.out.println("ABAIXO DO PESO");
+		} else if (imc >= 18.5 && imc <= 24.99) {
+            System.out.println("PESO IDEAL");
+		} else if (imc >= 25 && imc <= 29.99) {
+            System.out.println("ACIMA DO PESO");
+	    } else if (imc >= 30  && imc <= 34.99) {
+	        System.out.println("OBESIDADE 1");
+	    } else if (imc >= 35  && imc <= 39.99) {
+            System.out.println("OBESIDADE 2");
+        } else {
+            System.out.println("OBESIDADE 3");
+       }
+	}
+
 
 	}
-	
-
-	}
-
-}
